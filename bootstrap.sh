af94aa63d2415216e6dfa7b67ff054316064ad23
bigwheel <k.bigwheel+eng@gmail.com>
2016-12-23 13:00:31 +0900

    batsによるテストのテンプレートを追加

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+# provisioner: shell でkitchen testするとchefが入らない
+# https://github.com/test-kitchen/test-kitchen/pull/632
+# 問題の根が深いのでまだtest-kitchen側で解決していない
+#
+# よって先人に習い、bootstrap.shでrubyを入れることによりとりあえず解決する
+# http://qiita.com/sawanoboly/items/2874018f385dbe8562b1
+sudo apt-get -y install ruby
+
 echo $DROPBOX_ACCESS_TOKEN | /tmp/kitchen/data/setup-linux.sh
