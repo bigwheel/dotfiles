90c14764ccb0f0c0e267136ef33fab4e609afbfd
bigwheel <k.bigwheel+eng@gmail.com>
2016-12-25 11:59:21 +0900

    ディレクトリ構造を修正

--- setup-linux.sh
+++ setup-linux.sh
@@ -5,5 +5,5 @@ set -eux
 # http://qiita.com/yudoufu/items/48cb6fb71e5b498b2532
 script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 
-$script_dir/ssh/setup-ssh-files.sh
-$script_dir/homeshick/install.sh
+$script_dir/foundation/ssh/setup-ssh-files.sh
+$script_dir/foundation/homeshick/install.sh
