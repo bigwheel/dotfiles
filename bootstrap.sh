7232e47e8eab2fe7146d3cfa77f6fd097549c74d
k.bigwheel <k.bigwheel+eng@gmail.com>
2024-11-05 12:43:42 +0900

    bootstrapに初期コマンドを追加

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,3 +2,5 @@
 
 mkdir -p ~/.local/share/fish
 ln -s ~/Dropbox/linux/dotfiles/fish_history ~/.local/share/fish/fish_history
+
+fisher install 'jorgebucaran/fisher' '2m/fish-history-merge' 'jethrokuan/z' 'PatrickF1/fzf.fish' 'b4b4r07/enhancd' 'bigwheel/fish-gitpath'
